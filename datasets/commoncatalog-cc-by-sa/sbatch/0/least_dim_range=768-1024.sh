f7fa6620b33897edd7d144523aa32742558a95e2
hibikaze-git <hrk.ymgch@gmail.com>
2024-06-22 11:05:20 +0900

    sbatch修正

--- datasets/commoncatalog-cc-by-sa/sbatch/0/least_dim_range=768-1024.sh
+++ datasets/commoncatalog-cc-by-sa/sbatch/0/least_dim_range=768-1024.sh
@@ -15,8 +15,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate llava-jp
 
 cd /storage4/work/yamaguchi/create-data-for-vlm
-#bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1
-echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1" | at 01:42 22.06.2024
+bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1
+#echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1" | at 1:10 22.06.2024
 
 cd /storage4/work/yamaguchi/create-data-for-vlm/datasets/commoncatalog-cc-by-sa
 
