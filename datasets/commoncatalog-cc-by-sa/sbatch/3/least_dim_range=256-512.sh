760a73d1b242c4e0a0c70b40f956d5b5ddcdbd4d
hibikaze-git <hrk.ymgch@gmail.com>
2024-06-23 00:04:48 +0900

    sbatch追加

---
+++ datasets/commoncatalog-cc-by-sa/sbatch/3/least_dim_range=256-512.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Command line options go here
+#SBATCH --time=10:00:00
+#SBATCH --nodelist=slurm0-a3-ghpc-1
+#SBATCH --job-name=vlm_synthesis_data
+#SBATCH --output=sbatch_logs/3_least_dim_range=256-512.out
+#SBATCH --gpus-per-node=1
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=100GB
+#SBATCH --begin=2024-06-22T15:00:00
+
+# Command(s) goes here
+source ~/miniconda3/etc/profile.d/conda.sh
+conda activate llava-jp
+
+cd /storage4/work/yamaguchi/create-data-for-vlm
+bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-1 stop 1
+#echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-1 stop 1" | at 20:00 21.06.2024
+
+cd /storage4/work/yamaguchi/create-data-for-vlm/datasets/commoncatalog-cc-by-sa
+
+python synthesis_data.py ./commoncatalog-cc-by-sa-download/3/least_dim_range=256-512 ./images/3/least_dim_range=256-512 ./jsonl/3/least_dim_range=256-512
