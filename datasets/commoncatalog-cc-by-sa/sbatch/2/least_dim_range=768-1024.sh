c965eb57665d91fbe74a78ea6bf8824b2d90f723
hibikaze-git <hrk.ymgch@gmail.com>
2024-06-23 09:45:36 +0900

    sbatch変更

--- datasets/commoncatalog-cc-by-sa/sbatch/2/least_dim_range=768-1024.sh
+++ datasets/commoncatalog-cc-by-sa/sbatch/2/least_dim_range=768-1024.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Command line options go here
-#SBATCH --time=18:45:00
+#SBATCH --time=24:00:00
 #SBATCH --nodelist=slurm0-a3-ghpc-2
 #SBATCH --job-name=vlm_synthesis_data
 #SBATCH --output=sbatch_logs/2_least_dim_range=768-1024.out
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=100GB
-#SBATCH --begin=2024-06-22T06:00:06
+# --begin=2024-06-22T06:00:02
 
 # Command(s) goes here
 source ~/miniconda3/etc/profile.d/conda.sh
