6c03e15a34b6bca4c5f500941f6368589d17db1d
hibikaze-git <hrk.ymgch@gmail.com>
2024-06-24 23:56:52 +0900

    sbatch変更

--- datasets/commoncatalog-cc-by-sa/sbatch/3/least_dim_range=256-512.sh
+++ datasets/commoncatalog-cc-by-sa/sbatch/3/least_dim_range=256-512.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Command line options go here
-#SBATCH --time=4:45:00
-#SBATCH --nodelist=slurm0-a3-ghpc-0
+#SBATCH --time=5:45:00
+#SBATCH --nodelist=slurm0-a3-ghpc-3
 #SBATCH --job-name=vlm_synthesis_data
 #SBATCH --output=sbatch_logs/3_least_dim_range=256-512.out
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=24
 #SBATCH --mem=100GB
-#--begin=2024-06-22T15:00:00
+#SBATCH --begin=2024-06-24T19:00:00
 
 # Command(s) goes here
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate llava-jp
 
 cd /storage4/work/yamaguchi/create-data-for-vlm
-bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1
-#echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1" | at 20:00 21.06.2024
+bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-3 stop 1
+#echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-3 stop 1" | at 20:00 21.06.2024
 
 cd /storage4/work/yamaguchi/create-data-for-vlm/datasets/commoncatalog-cc-by-sa
 
