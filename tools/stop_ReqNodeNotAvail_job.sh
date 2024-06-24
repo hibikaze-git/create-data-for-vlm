b7876b183b6f23d7967cb893cb475c4b0f8dcd41
hibikaze-git <hrk.ymgch@gmail.com>
2024-06-24 09:50:57 +0900

    止めるjob数を指定できるよう変更

--- tools/stop_ReqNodeNotAvail_job.sh
+++ tools/stop_ReqNodeNotAvail_job.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # 引数のチェック
-if [ "$#" -ne 1 ]; then
-    echo "使用法: $0 <ノード名>"
+if [ "$#" -ne 2 ]; then
+    echo "使用法: $0 <ノード名> <GPU数>"
     exit 1
 fi
 
 # 引数を変数に格納
 node_name=$1
+gpu_num=$2
 
 while true; do
     # `squeue` の出力を変数に格納
@@ -16,7 +17,7 @@ while true; do
     # 条件をチェック
     if echo "$squeue_output" | grep -q "0618cc_.*ext_kan_.*ReqNodeNotAvail.*UnavailableNodes:${node_name}"; then
         # 条件に一致した場合にスクリプトを実行
-        bash tools/update_0618cc_gpu_num.sh ${node_name} stop 8
+        bash tools/update_0618cc_gpu_num.sh ${node_name} stop ${gpu_num}
     else
         # 条件に一致しない場合はループを終了
         echo "条件に一致しません。スクリプトを終了します。"
