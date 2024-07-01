cfd1de266c49b9e54957321c9d3f210e33a15b69
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-01 09:48:16 +0900

    sbatch更新

--- datasets/text2image/sbatch/wikipedia-22-12-ja-embeddings.sh
+++ datasets/text2image/sbatch/wikipedia-22-12-ja-embeddings.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Command line options go here
-#SBATCH --time=6:00:00
-#SBATCH --nodelist=slurm0-a3-ghpc-15
+#SBATCH --time=13:45:00
+#SBATCH --nodelist=slurm0-a3-ghpc-0
 #SBATCH --job-name=vlm_t2h
 #SBATCH --output=sbatch_logs/wikipedia-22-12-ja-embeddings.out
 #SBATCH --gpus-per-node=1
@@ -15,8 +15,8 @@ source ~/miniconda3/etc/profile.d/conda.sh
 conda activate llava-jp
 
 cd /storage4/work/yamaguchi/create-data-for-vlm
-bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-15 stop 1
-#echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-15 stop 1" | at 1:10 22.06.2024
+bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1
+#echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1" | at 1:10 22.06.2024
 
 cd /storage4/work/yamaguchi/create-data-for-vlm/datasets/text2image
 
