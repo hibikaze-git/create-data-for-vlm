cf511f3421edb82c6438ac248c8701b41fa7e323
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-01 19:47:29 +0900

    jsonlのアップロード時にサブセット名を指定できるよう変更

--- tools/upload_hf_hub.py
+++ tools/upload_hf_hub.py
@@ -3,6 +3,7 @@ jsonlをHFにアップロード
 
 huggingface-cli login
 python tools/upload_hf_hub.py ./datasets/commoncatalog-cc-by-sa/jsonl/0/part-00000-tid-7761264679195244086-4c6ec9dc-fb3d-4bd3-98ec-f987466013dd-3488916-1-c000.jsonl hibikaze/upload_test
+python tools/upload_hf_hub.py ./datasets/commoncatalog-cc-by-sa/jsonl/0/part-00000-tid-7761264679195244086-4c6ec9dc-fb3d-4bd3-98ec-f987466013dd-3488916-1-c000.jsonl hibikaze/upload_test subset_name
 """
 
 import argparse
@@ -34,11 +35,15 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("input_file", type=str, help="Input JSONL file containing text data.")
 parser.add_argument("repo_name", type=str, help="hugging face repository name")
+parser.add_argument("subset_name", type=str, help="hugging face repository subset name")
 
 args = parser.parse_args()
 
 dataset = load_dataset("json", data_files=args.input_file, split="train", cache_dir="./dataset_cache")
 
-dataset.push_to_hub(args.repo_name, private=True)
+if args.subset_name:
+    dataset.push_to_hub(args.repo_name, args.subset_name, private=True)
+else:
+    dataset.push_to_hub(args.repo_name, private=True)
 
 rm_cache()
