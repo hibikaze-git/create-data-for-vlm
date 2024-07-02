4d1787e380401d4fbdf01ad04d62562a0082da89
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-02 22:41:39 +0900

    pngもカウントするよう変更

--- tools/count_jpg_files.sh
+++ tools/count_jpg_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # 引数が指定されているか確認
-# bash count_jpg_files.sh ./images
+# bash count_image_files.sh ./images
 if [ $# -ne 1 ]; then
   echo "Usage: $0 <directory>"
   exit 1
@@ -16,5 +16,13 @@ fi
 # .jpgファイルの数をカウント
 jpg_count=$(find "$1" -type f -name "*.jpg" | wc -l)
 
+# .pngファイルの数をカウント
+png_count=$(find "$1" -type f -name "*.png" | wc -l)
+
+# .jpgと.pngファイルの合計数を計算
+total_count=$((jpg_count + png_count))
+
 # 結果を表示
 echo "Total .jpg files in directory $1: $jpg_count"
+echo "Total .png files in directory $1: $png_count"
+echo "Total image files (.jpg + .png) in directory $1: $total_count"
