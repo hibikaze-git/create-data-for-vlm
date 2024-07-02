0cee790c376b06b587c0532a43291fe53ed8333d
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-03 07:02:42 +0900

    sbatch更新

--- datasets/complex_reasoning/sbatch/4/least_dim_range=256-512.sh
+++ datasets/complex_reasoning/sbatch/4/least_dim_range=256-512.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Command line options go here
-#SBATCH --time=10:00:00
+#SBATCH --time=4:45:00
 #SBATCH --nodelist=slurm0-a3-ghpc-15
 #SBATCH --job-name=vlm_synthesis_data
 #SBATCH --output=sbatch_logs/4_least_dim_range=256-512.out
