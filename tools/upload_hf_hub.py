a433167802a8aab8c408b855e5bb55012f1e4a06
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-06 00:37:41 +0900

    バグ修正

--- tools/upload_hf_hub.py
+++ tools/upload_hf_hub.py
@@ -3,7 +3,7 @@ jsonlをHFにアップロード
 
 huggingface-cli login
 python tools/upload_hf_hub.py ./datasets/commoncatalog-cc-by-sa/jsonl/0/part-00000-tid-7761264679195244086-4c6ec9dc-fb3d-4bd3-98ec-f987466013dd-3488916-1-c000.jsonl hibikaze/upload_test
-python tools/upload_hf_hub.py ./datasets/commoncatalog-cc-by-sa/jsonl/0/part-00000-tid-7761264679195244086-4c6ec9dc-fb3d-4bd3-98ec-f987466013dd-3488916-1-c000.jsonl hibikaze/upload_test subset_name
+python tools/upload_hf_hub.py ./datasets/commoncatalog-cc-by-sa/jsonl/0/part-00000-tid-7761264679195244086-4c6ec9dc-fb3d-4bd3-98ec-f987466013dd-3488916-1-c000.jsonl hibikaze/upload_test --subset_name subset_name
 """
 
 import argparse
@@ -35,7 +35,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("input_file", type=str, help="Input JSONL file containing text data.")
 parser.add_argument("repo_name", type=str, help="hugging face repository name")
-parser.add_argument("subset_name", type=str, help="hugging face repository subset name", default=None)
+parser.add_argument("--subset_name", type=str, help="hugging face repository subset name", default=None)
 
 args = parser.parse_args()
 
