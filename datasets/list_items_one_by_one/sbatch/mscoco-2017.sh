e90e3628e5d29e43f6a1df9ff208082af8716e9a
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-11 22:14:53 +0900

    sbatch修正

--- datasets/list_items_one_by_one/sbatch/mscoco-2017.sh
+++ datasets/list_items_one_by_one/sbatch/mscoco-2017.sh
@@ -14,10 +14,10 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate llava-jp-2
 
-cd /storage4/work/yamaguchi/create-data-for-vlm
+cd /storage5/multimodal/work/yamaguchi/create-data-for-vlm
 bash tools/update_0705cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1
 #echo "bash tools/update_0618cc_gpu_num.sh slurm0-a3-ghpc-0 stop 1" | at 1:10 22.06.2024
 
-cd /storage4/work/yamaguchi/create-data-for-vlm/datasets/list_items_one_by_one
+cd /storage5/multimodal/work/yamaguchi/create-data-for-vlm/datasets/list_items_one_by_one
 
 python synthesis_data.py ./train2017 ./images ./jsonl
