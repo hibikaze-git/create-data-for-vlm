925506212e48f6a68c47109e61dd0e46ab230abe
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-12 21:30:17 +0900

    データダウンロード用スクリプト修正

--- datasets/list_items_one_by_one/download_mscoco.sh
+++ datasets/list_items_one_by_one/download_mscoco.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-wget -P http://images.cocodataset.org/zips/train2017.zip
+wget -P . http://images.cocodataset.org/zips/train2017.zip
+wget -P . http://images.cocodataset.org/annotations/annotations_trainval2017.zip
+
+unzip ./train2017.zip
+unzip ./annotations_trainval2017.zip
