69413406600b60ae946f6fc6f80ca53c9bd39b35
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-12 21:27:29 +0900

    データ形式変換用コード追加

---
+++ datasets/list_items_one_by_one/convert_jsonl.py
@@ -0,0 +1,39 @@
+import json
+
+
+def read_jsonl(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return [json.loads(line) for line in file]
+
+
+def transform_data(data):
+    labels = []
+    bboxs = []
+    centers = []
+
+    for key, value in data["labels"].items():
+        labels.append(value["label"])
+        bboxs.append(value["bbox"])
+        centers.append((value["center_x"], value["center_y"]))
+
+    return {
+        "labels": labels,
+        "bboxs": bboxs,
+        "centers": centers,
+        "detailed": data["detailed"],
+        "id": data["id"],
+        "ext": data["ext"],
+    }
+
+
+def save_to_jsonl(data_list, output_file_path):
+    with open(output_file_path, "w", encoding="utf-8") as file:
+        for data in data_list:
+            file.write(json.dumps(data) + "\n")
+
+
+jsonl_data = read_jsonl("./all_0712.jsonl")
+
+transformed_data_list = [transform_data(data) for data in jsonl_data]
+
+save_to_jsonl(transformed_data_list, "./all_0712.jsonl")
