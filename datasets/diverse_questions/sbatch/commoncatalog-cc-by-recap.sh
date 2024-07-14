4f6b5b22c64b40af849327a2d1c867561f7cff69
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-14 10:38:37 +0900

    sbatch更新

--- datasets/diverse_questions/sbatch/commoncatalog-cc-by-recap.sh
+++ datasets/diverse_questions/sbatch/commoncatalog-cc-by-recap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Command line options go here
-#SBATCH --time=7:00:00
+#SBATCH --time=23:00:00
 #SBATCH --nodelist=slurm0-a3-ghpc-0
 #SBATCH --job-name=vlm_synthesis_data
 #SBATCH --output=sbatch_logs/commoncatalog-cc-by-recap.out
