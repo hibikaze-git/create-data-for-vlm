35ec47055dbca0f1e06f124e73f89a17f33b4c8a
hibikaze-git <hrk.ymgch@gmail.com>
2024-07-16 23:23:37 +0900

    バグ修正

--- datasets/wit/download_wit_base.py
+++ datasets/wit/download_wit_base.py
@@ -1,3 +1,8 @@
 from huggingface_hub import snapshot_download
 
-download_path = snapshot_download(repo_id="wikimedia/wit_base", cache_dir="./cache", local_dir="./wit_base")
+download_path = snapshot_download(
+    repo_id="wikimedia/wit_base",
+    repo_type="dataset",
+    cache_dir="./cache",
+    local_dir="./wit_base",
+)
